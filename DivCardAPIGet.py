9eab7dc317d7a8c152f4447687bf481977e2aafb
Brianw9 <32607411+Brianw9@users.noreply.github.com>
2019-02-06 13:07:22 -0500

    Update DivCardAPIGet.py

--- DivCardAPIGet.py
+++ DivCardAPIGet.py
@@ -2,6 +2,7 @@ import requests
 import json
 import copy
 import csv
+import pandas
 
 leagueName = "Betrayal"                                                                             #League name easily to update
 divCardAPI = "http://poe.ninja/api/Data/GetDivinationCardsOverview?league=" + leagueName
@@ -18,26 +19,24 @@ divCardListofDict = []                                                          
 
 #Hand picked cards with parseable outcomes
 goodDivCards = ['A Dab of Ink', 'A Mother\'s Parting Gift', 'Anarchy\'s Price', 'Audacity', 'Beauty Through Death', 'Death', 'Forbidden Power', 'Humility',
-                'Hunter\'s Reward', 'Immortal Resolve', 'Last Hope', 'Lingering Remnants', 'Might is Right', 'Pride Before the Fall', 'Rats', 'Rebirth', 'Scholar of the Seas',
-                'The Arena Champion', 'The Army of Blood','The Artist','The Beast','The Betrayal','The Blazing Fire','The Brittle Emperor','The Coming Storm',
-                'The Conduit','The Cursed King','The Darkest Dream','The Demoness', 'The Doctor','The Dragon','The Dragon\'s Heart', 'The Dreamland','The Drunken Aristocrat',
+                'Hunter\'s Reward', 'Immortal Resolve', 'Might is Right', 'Pride Before the Fall', 'Rats', 'Rebirth',
+                'The Army of Blood','The Artist','The Beast','The Betrayal','The Blazing Fire','The Brittle Emperor','The Coming Storm',
+                'The Conduit','The Cursed King','The Darkest Dream','The Demoness', 'The Doctor','The Dragon','The Dragon\'s Heart','The Drunken Aristocrat',
                 'The Endless Darkness', 'The Enlightened', 'The Fathomless Depths','The Feast','The Fiend','The Fletcher','The Formless Seas','The Forsaken', 'The Harvester',
                 'The Hermit', 'The Hunger', 'The Immortal', 'The Incantation', 'The Iron Bard', 'The Jeweller\'s Boon', 'The King\'s Heart', 'The Last One Standing', 'The Life Thief',
-                'The Lunaris Priestess', 'The Master', 'The Mayor', 'The Nurse', 'The Oath', 'The Offering', 'The One With All', 'The Pack Leader','The Pact','The Polymath','The Professor',
-                'The Queen', 'The Scarred Meadow', 'The Scavenger','The Soul', 'The Spark and the Flame', 'The Sun', 'The Sword King\'s Salute', 'The Throne', 'The Twilight Moon',
+                'The Lunaris Priestess', 'The Master', 'The Nurse', 'The Oath', 'The Offering', 'The One With All', 'The Pack Leader','The Pact','The Polymath',
+                'The Queen', 'The Scarred Meadow', 'The Scavenger','The Soul', 'The Spark and the Flame', 'The Sun', 'The Sword King\'s Salute', 'The Throne',
                 'The Valley of Steel Boxes','The Watcher','The Wind', 'The Witch','The Wolf\'s Shadow', 'The Wolven King\'s Bite', 'The World Eater', 'Thunderous Skies', 'Tranquility', 'Wealth and Power']
 
 
 
 class APIGet:
     divCardAPIResponse = json.loads(requests.get(divCardAPI).text)  #Calls for the divCardAPI
-    prophecyAPIResponse = json.loads(requests.get(prophecyAPI).text)
-    fragmentAPIResponse = json.loads(requests.get(fragmentAPI).text)
-    uniqueMapAPIResponse = json.loads(requests.get(uniqueMapAPI).text)
+    # prophecyAPIResponse = json.loads(requests.get(prophecyAPI).text)
     uniqueAccessoryAPIResponse = json.loads(requests.get(uniqueAccessoryAPI).text)
-    uniqueArmourAPIResponse = json.loads(requests.get(uniqueArmourAPI).text)
-    uniqueFlaskAPIResponse = json.loads(requests.get(uniqueFlaskAPI).text)
-    uniqueWeaponAPIResponse = json.loads(requests.get(uniqueWeaponAPI).text)
+    # uniqueArmourAPIResponse = json.loads(requests.get(uniqueArmourAPI).text)
+    # uniqueFlaskAPIResponse = json.loads(requests.get(uniqueFlaskAPI).text)
+    # uniqueWeaponAPIResponse = json.loads(requests.get(uniqueWeaponAPI).text)
 
 class DivCardValues:
     def __init__(self, cardname, itemname, value, corrupted):       #setup for appending deserialized values to dictionaries
@@ -46,6 +45,11 @@ class DivCardValues:
         self.value = value
         self.corrupted = corrupted
 
+class ActualItemValue:
+    def __init__(self, itemname, item_value, corrupted):
+        self.itemname = itemname
+        self.item_value = item_value
+        self.corrupted = corrupted
 
 
 for i in APIGet.divCardAPIResponse['lines']:           #parse through initial API list
@@ -65,13 +69,17 @@ for i in APIGet.divCardAPIResponse['lines']:           #parse through initial AP
         divCardListofDict += [copy.deepcopy(divCardDict)]               #appending the temporary dictionary to the global list of DivCard dictionaries
 
 with open('divcards.csv', 'w') as csv_file:
-    fieldnames = ['cardname', 'itemname', 'value','corrupted', 'item_value']
+    fieldnames = ['cardname', 'itemname', 'value','corrupted', 'item_value']   #converts list of dictionaries to a CSV, adds blank field for actual item value.
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
     for i in divCardListofDict:
         writer.writerow(i)
+colnames = ['cardname','itemname','value','corrupted','item_value']
+data = pandas.read_csv('divcards.csv', names=colnames)
+itemname = data.itemname.tolist()
 
-
-
+for i in APIGet.uniqueAccessoryAPIResponse['lines']:
+    if i['name'] in itemname:
+        print(i['name'])
 
 
