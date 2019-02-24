73ac847b3e85240b37e387ecc64cdcec35dfbac2
Brianw9 <32607411+Brianw9@users.noreply.github.com>
2019-02-23 21:18:49 -0500

    Update ApexSim.py

--- ApexSim.py
+++ ApexSim.py
@@ -9,7 +9,7 @@ print("Are you ready to be the Apex Champion?")
 time.sleep(2)
 print("Choose Your Squad.")
 
-Legends = {"Bloodhound":0.1,"Gibraltar":0.05, "Lifeline":0.15, "Pathfinder":0.1, "Wraith":0.15, "Bangalore":0.25, "Caustic":0.07, "Mirage":0.1, "Hyper":1}
+Legends = {"Bloodhound":0.1,"Gibraltar":0.1, "Lifeline":0.15, "Pathfinder":0.15, "Wraith":0.30, "Bangalore":0.5, "Caustic":0.14, "Mirage":0.20, "Hyper":.80}
 #Legends to choose from and their individual win multiplier
 
 squad1 = ""
@@ -20,17 +20,27 @@ HighTierLoot = ["Artillery","Relay","The Pit","Runoff","Bunker","Airbase","Swamp
 MidTierLoot = ["Slum Lakes", "Cascades","Wetlands","Bridges","Skull Town"]
 LowTierLoot = ["Hydro Dam","Market"]
 
+GoodWeapons = ["Wingman", "Spitfire", "R-99", "Peacekeeper", "EVA-8", "Prowler", "R-301"]
+OkayWeapons = ["Flatline", "RE-45", "G7 Scout", "Hemlok", "Longbow", "Devotion"]
+BadWeapons = ["Triple Take", "Alternator", "P2020", "Mozambique"]
+RareWeapons = ["Mastiff", "Kraber"]
+
+Weapon1 = ""
+Weapon2 = ""
+
 dropArea = randint(0,9) #variables required for the next multiplier
 dropAreaMulti = 0
-baseline_squad_multi = 0
+droppodLuck = 0.1
+weaponTierMulti = 0
+baselineSquadMulti = 0
 SquadAll = []  #Keeps a list of the squad names, could be useful later
-
+fights_won = []
 
 def choose_squad():
     global squad1
     global squad2
     global squad3
-    global baseline_squad_multi
+    global baselineSquadMulti
     global SquadAll
     squad1 = input("First Legend: (Type \'list\' for all options)")
     while squad1 == 'list' or squad1 not in Legends:
@@ -47,15 +57,15 @@ def choose_squad():
     SquadAll.append(squad1)
     SquadAll.append(squad2)
     SquadAll.append(squad3)
-    baseline_squad_multi = Legends[squad1] + Legends[squad2] + Legends[squad3]
+    baselineSquadMulti = Legends[squad1] + Legends[squad2] + Legends[squad3]
 
 
 def get_dropzone():
     global dropAreaMulti
     if dropArea <= 2:
         time.sleep(1)
-        print("You\'re dropping in: " + LowTierLoot[randint(0,1)] + "!")
-        dropAreaMulti = 0.1
+        print("You\'re dropping in: " + LowTierLoot[randint(0,1)] + "!") #gets a random drop zone, builds suspense for better drop zones
+        dropAreaMulti = 0.1                                              #sets a constant variable for the multiplier of loot tier.
     if  2 < dropArea <= 6:
         time.sleep(1)
         print("You\'re dropping in: " + MidTierLoot[randint(0,4)] + "!")
@@ -65,17 +75,73 @@ def get_dropzone():
         print("You\'re dropping in: " + HighTierLoot[randint(0,9)] + "!")
         dropAreaMulti = 0.5
 
+def get_win_fight(x,y):
+    i = 0
+    maxes = {
+        0.5:3,
+        0.25:2,
+        0.1:1,
+    }
+    global fights_won
+
+    while i < maxes[x]:
+        if randint(0,100) <= (y * 100):
+            fights_won.append(True)
+            i += 1
+        else:
+            fights_won.append(False)
+            i += 1
+
+def hasWins():
+    if fights_won.count(False) >= 1:
+        return 0
+    if fights_won.count(True)>= 0 and fights_won.count(False) == 0:
+        return fights_won.count(True)
 
 
 
+def get_droppod_luck(z):
+    global droppodLuck
+    if "Lifeline"in SquadAll:
+        droppodLuck += 0.2
+    droppodLuck += z/10
 
 
 
-choose_squad();
+def get_weapon_tier():
+    global weaponTierMulti
+    global Weapon1
+    global Weapon2
+    if dropAreaMulti == 0.1:
+        Weapon1 = OkayWeapons[randint(0,5)]
+        Weapon2 = BadWeapons[randint(0,3)]
+    if dropAreaMulti == 0.25:
+        Weapon1 = GoodWeapons[randint(0,6)]
+        Weapon2 = OkayWeapons[randint(0,5)]
+    if dropAreaMulti == 0.5:
+        Weapon1 = GoodWeapons[randint(0,6)]
+        Weapon2 = GoodWeapons[randint(0,5)]
+    if droppodLuck >= 0.4:
+        Weapon1 = RareWeapons[randint(0,1)]
+    print("You found a {0}, and {1}".format(Weapon1,Weapon2))
+
+def was_champion():
+    if fights_won.count(False) == 0:
+        return "were"
+    else:
+        return "were not"
+
+choose_squad()
 time.sleep(2)
 print("The squad is all set, lets see where we're dropping.")
-get_dropzone();
-print(SquadAll)
+get_dropzone()
+get_droppod_luck(fights_won.count(True))
+get_win_fight(dropAreaMulti, baselineSquadMulti)
+get_weapon_tier()
+time.sleep(2)
+print("You won {} fights before the final circle".format(hasWins()))
+time.sleep(3)
+print("You {} the Apex Champion this time.".format(was_champion()))
 
 
 
